fb578fc47bcbbaa64a39de9602ebabb94245f38a
Adeline Su <su.adeline@gmail.com>
2025-05-19 19:15:23 -0400

    add auth using supabase

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { supabase } from "../lib/supabase"; // Adjust path as needed
+import Auth from "../components/Auth"; // Adjust path as needed
+import { Session } from "@supabase/supabase-js";
+
 import Home from "./Home";
-import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import Playlist from "./Playlist";
 import Settings from "./Settings";
-import { NavigationContainer } from "@react-navigation/native";
-// import { CurvedBottomBarExpo } from 'react-native-curved-bottom-bar';
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 
 const Tab = createBottomTabNavigator();
 
 export default function RootLayout() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  if (!session) {
+    // If no session, show auth screen
+    return <Auth />;
+  }
+
+  // If user is logged in, show bottom tab navigator
   return (
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={{
           tabBarShowLabel: false,
           tabBarActiveTintColor: "#09acbd",
-          animation: "shift"
+          animation: "shift",
         }}
       >
         <Tab.Screen
