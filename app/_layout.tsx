5f80de7a9d304c8a87f6d2042feef9e9fb4be809
Adeline Su <su.adeline@gmail.com>
2025-05-20 09:20:26 -0400

    use absolute paths

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import { View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import { supabase } from "../lib/supabase"; // Adjust path as needed
-import Auth from "../components/Auth"; // Adjust path as needed
+import { supabase } from "@/lib/supabase"; 
+import Auth from "@/components/Auth";
 import { Session } from "@supabase/supabase-js";
 
-import Home from "./Home";
-import Playlist from "./Playlist";
-import Settings from "./Settings";
+import Home from "@/app/Home";
+import Playlist from "@/app/Playlist";
+import Settings from "@/app/Settings";
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 
 const Tab = createBottomTabNavigator();
